c2971b67877d644135357a389b09bb0515a1ab01
Roberto <161033388+Hubierto@users.noreply.github.com>
2024-10-08 14:21:44 +0000

    ok

--- projeto/tests/test_funcionario.py
+++ projeto/tests/test_funcionario.py
@@ -11,31 +11,31 @@ def funcionario_valido():
     funcionario = Funcionario(111, "a", "1111-2222", "a@gmail.com", Endereco("Rua A", "1", "Térreo", "1111", "São Paulo", UnidadeFederativa.SAO_PAULO), Sexo.FEMININO, EstadoCivil.SOLTEIRO, "01/01/01", "1111-1111", "2222-2222", "3333", Setor.ENGENHARIA, 6000)
     return funcionario
 
-def test_salario_valido(funcionario_valido):
+def test_salario_valido(funcionario_valido: Funcionario):
     assert funcionario_valido.salario == 6000
 
-def test_mudar_salario_valido(funcionario_valido):
+def test_mudar_salario_valido(funcionario_valido: Funcionario):
     funcionario_valido.salario = 7000
     assert funcionario_valido.salario == 7000
 
-def test_cpf_valido(funcionario_valido):
+def test_cpf_valido(funcionario_valido: Funcionario):
     assert funcionario_valido.cpf == "1111-1111"
 
-def test_mudar_cpf_valido(funcionario_valido):
+def test_mudar_cpf_valido(funcionario_valido: Funcionario):
     funcionario_valido.cpf = "2222-2222"
     assert funcionario_valido.cpf == "2222-2222"
 
-def test_rg_valido(funcionario_valido):
+def test_rg_valido(funcionario_valido: Funcionario):
     assert funcionario_valido.rg == "2222-2222"
 
-def test_mudar_rg_valido(funcionario_valido):
+def test_mudar_rg_valido(funcionario_valido: Funcionario):
     funcionario_valido.rg = "3333-3333"
     assert funcionario_valido.rg == "3333-3333"
 
-def test_matricula_valido(funcionario_valido):
-    assert funcionario_valido.matricula == "3333"
+def test_matricula_valido(funcionario_valido: Funcionario):
+    assert funcionario_valido.matricula == 3333
 
-def test_mudar_matricula_valido(funcionario_valido):
+def test_mudar_matricula_valido(funcionario_valido: Funcionario):
     funcionario_valido.matricula = "4444"
     assert funcionario_valido.matricula == "4444"
 
