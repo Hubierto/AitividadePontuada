ea15c1fa3bfb7552a07bcf4182c91da3a925ba80
Roberto <161033388+Hubierto@users.noreply.github.com>
2024-10-08 14:02:31 +0000

    foi

--- projeto/main.py
+++ projeto/main.py
@@ -1,7 +1,15 @@
 import os
+print(os.getcwd())
+import sys
+sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+ 
+from projeto.models.medico import Medico
+from projeto.models.endereco import Endereco
+from projeto.models.enums.unidade_federativa import UnidadeFederativa 
+from projeto.models.enums.estado_civil import EstadoCivil
+from projeto.models.enums.sexo import Sexo
+from projeto.models.enums.setor import Setor
 
 os.system("cls || clear")
 
-#Instanciando classe
-if __name__ == "__main__":
-    os.system("pytest")
+medico1 = Medico(999, "Ray", "71988228822", "ray@gmail.com", Endereco("praça do sol", "1230", "E", "61243", "Salvador", UnidadeFederativa.BAHIA), Sexo.FEMININO, EstadoCivil.CASADO, "01/04/2002","12354354212", "85890238985", "0i9090909", Setor.ENGENHARIA, 100.000, "10.000")
